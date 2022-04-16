2c17171398de5a4c245672bb852abfb044939771
sudhakarmako <sudhakar@makoitlab.com>
2022-04-17 02:23:55 +0530

    fix: :lipstick: Row File updated
    Row File updated

--- src/@ui/Row/Row.tsx
+++ src/@ui/Row/Row.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Row.scss";
 
-const Row = ({ children }: { children: React.ReactNode }) => {
-  return <div className="row">{children}</div>;
+type RowType = {
+  children: React.ReactNode;
+  justify?:
+    | "center"
+    | "flex-start"
+    | "flex-end"
+    | "space-between"
+    | "space-around"
+    | "space-evenly";
+  align?: "center" | "flex-start" | "flex-end";
+};
+
+const Row = ({ children, justify, align }: RowType) => {
+  let RowClassName = "row";
+  RowClassName += !!justify ? ` justify-${justify}` : "";
+  RowClassName += !!align ? ` align-${align}` : "";
+
+  return <div className={RowClassName}>{children}</div>;
 };
 export default Row;
