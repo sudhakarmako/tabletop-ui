6e10e7d9896d6e98fc29fa7fc5f83845c1647176
sudhakarmako <sudhakar@makoitlab.com>
2022-04-17 02:24:29 +0530

    feat: :sparkles: Chip Component
    Chip Component

---
+++ src/@ui/Chip/Chip.tsx
@@ -0,0 +1,23 @@
+import "./Chip.scss";
+
+type ChipInteface = {
+  image?: string;
+  children?: string;
+};
+const Chip = ({ image, children }: ChipInteface) => {
+  return (
+    <div className="chip-container">
+      {!!image && <img className="chip-image" src={image} alt="chip-image" />}
+      <div className="chip-content">
+        {children && children?.length > 26
+          ? children?.substring(0, 26) + "..."
+          : children}
+      </div>
+      <div className="chip-close">
+        <i className="bi bi-x-lg"></i>
+      </div>
+    </div>
+  );
+};
+
+export default Chip;
--- src/@ui/index.ts
+++ src/@ui/index.ts
@@ -2,3 +2,4 @@ export { Container } from "./Container";
 export { Button } from "./Button";
 export { Card } from "./Card";
 export { Row } from "./Row";
+export { Chip } from "./Chip";
