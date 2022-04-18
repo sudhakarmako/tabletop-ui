65161ccfb106b08c439d95b59466e00745594e24
sudhakarmako <sudhakar@makoitlab.com>
2022-04-18 18:04:18 +0530

    Session import

--- src/@components/index.ts
+++ src/@components/index.ts
@@ -2,3 +2,4 @@ export { AppBar } from "./AppBar";
 export { GameCard } from "./GameCard";
 export { SessionCard } from "./SessionCard";
 export { PlayerCard } from "./PlayerCard";
+export { SessionTitle } from "./SessionTitle";
