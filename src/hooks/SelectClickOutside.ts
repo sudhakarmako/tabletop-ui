1c8da63e9725c24a2b628b01606341d442988621
sudhakarmako <sudhakar@makoitlab.com>
2022-04-18 18:05:34 +0530

    Select click outside

---
+++ src/hooks/SelectClickOutside.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+export function useSelectOutside(ref: any, cb: () => void) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        cb();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
