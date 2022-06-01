ad054951456edce6bd768f515c5dae20e289954d
sudhakarmako <sudhakar@makoitlab.com>
2022-06-01 16:24:38 +0530

    Adding 'type' to button

--- src/@ui/Button/Button.tsx
+++ src/@ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import "./Button.scss";
-const Button = ({ children }: { children: ReactNode }) => {
-  return <button className="button">{children}</button>;
+const Button = ({ children, type }: { children: ReactNode, type?:"button" | "submit" | "reset" | undefined }) => {
+  return <button className="button" type={type}>{children}</button>;
 };
 export default Button;
