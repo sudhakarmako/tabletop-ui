30ffd42bff76ffd7db409c0ef614b32763287182
sudhakarmako <sudhakar@makoitlab.com>
2022-06-02 16:50:55 +0530

    session slice

--- src/store/actions/session.actions.ts
+++ src/store/actions/session.actions.ts
@@ -1,5 +1,77 @@
-export const getSessionsAction = () => console.log('getSessionsActions')
-export const getSessionAction = () => console.log('getSessionActions')
-export const addSessionAction = () => console.log('addSessionActions')
-export const updateSessionAction = () => console.log('updateSessionActions')
-export const deleteSessionAction = () => console.log('deleteSessionActions')
+import { sessionAPI } from "@api";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+//GET ALL SESSIONS
+export const getSessionsAction = createAsyncThunk(
+  "player/getPlayers",
+  async (payload: string | "", { rejectWithValue }) => {
+    let req = [];
+    try {
+      req = await sessionAPI.getSessions(payload);
+    } catch (error) {
+      req = [];
+      rejectWithValue(error);
+    }
+    return req;
+  }
+);
+
+// GET SINGLE SESSION
+export const getSessionAction = createAsyncThunk(
+  "player/getPlayers",
+  async (payload: number, { rejectWithValue }) => {
+    let req = [];
+    try {
+      req = await sessionAPI.getSessionById(payload);
+    } catch (error) {
+      req = [];
+      rejectWithValue(error);
+    }
+    return req;
+  }
+);
+
+// ADD SESSION SINGLE
+export const addSessionAction = createAsyncThunk(
+  "player/addPlayer",
+  async (payload: {}, { rejectWithValue }) => {
+    let req = [];
+    try {
+      req = await sessionAPI.createSessions(payload);
+    } catch (error) {
+      req = [];
+      rejectWithValue(error);
+    }
+    return req;
+  }
+);
+
+// UPDATE SINGLE SESSION
+export const updateSessionAction = createAsyncThunk(
+  "player/updatePlayer",
+  async (payload: {id:number}, { rejectWithValue }) => {
+    let req:any = [];
+    try {
+      req = await sessionAPI.updateSessions(payload.id, payload);
+    } catch (error) {
+      req = [];
+      rejectWithValue(error);
+    }
+    return req;
+  }
+);
+
+// DELETE SESSION SINGLE
+export const deleteSessionAction = createAsyncThunk(
+  "player/deletePlayer",
+  async (payload: {id:number}, { rejectWithValue }) => {
+    let req:any = [];
+    try {
+      req = await sessionAPI.deleteSessionById(payload.id);
+    } catch (error) {
+      req = [];
+      rejectWithValue(error);
+    }
+    return req;
+  }
+);
--- src/store/slices/session.slice.ts
+++ src/store/slices/session.slice.ts
@@ -1,35 +1,96 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { addSessionAction, deleteSessionAction, getSessionAction, getSessionsAction, updateSessionAction } from "store/actions/session.actions";
+import {
+  addSessionAction,
+  deleteSessionAction,
+  getSessionAction,
+  getSessionsAction,
+  updateSessionAction,
+} from "store/actions/session.actions";
 
 export const sessionSlice = createSlice({
   name: "session",
   initialState: {
     sessions: [],
+    status:"idle"
   },
   reducers: {
     getSessions: (state, action: PayloadAction<string>) => {
       // GET ALL SESSIONS
-      return getSessionsAction()
     },
     getSession: (state, action: PayloadAction<number>) => {
       // GET SINGLE SESSIONS
-      return getSessionAction();
     },
     addSession: (state, action: PayloadAction<object>) => {
       // ADDING A SINGLE SESSIONS
-      return addSessionAction()
     },
     updateSession: (state, action: PayloadAction<object>) => {
       // UPDATE SINGLE SESSIONS
-      return updateSessionAction();
     },
-    deleteSession: (state, action:PayloadAction<number>) => {
+    deleteSession: (state, action: PayloadAction<number>) => {
       // DELTE SINGLE SESSIONS
-      return deleteSessionAction();
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSessionsAction.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getSessionsAction.fulfilled, (state, action) => {
+        state.sessions = action.payload;
+        state.status = "idle";
+      })
+      .addCase(getSessionsAction.rejected, (state, action) => {
+        state.status = "rejected";
+      });
+    builder
+      .addCase(getSessionAction.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(getSessionAction.fulfilled, (state, action) => {
+        state.status = "idle";
+      })
+      .addCase(getSessionAction.rejected, (state) => {
+        state.status = "rejected";
+      });
+    builder
+      .addCase(addSessionAction.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(addSessionAction.fulfilled, (state) => {
+        state.status = "idle";
+      })
+      .addCase(addSessionAction.rejected, (state) => {
+        state.status = "rejected";
+      });
+    builder
+      .addCase(updateSessionAction.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(updateSessionAction.fulfilled, (state) => {
+        state.status = "idle";
+      })
+      .addCase(updateSessionAction.rejected, (state) => {
+        state.status = "rejected";
+      });
+    builder
+      .addCase(deleteSessionAction.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(deleteSessionAction.fulfilled, (state) => {
+        state.status = "idle";
+      })
+      .addCase(deleteSessionAction.rejected, (state) => {
+        state.status = "rejected";
+      });
   },
 });
 
-export const {getSessions, getSession, addSession, updateSession, deleteSession} = sessionSlice.actions;
+export const {
+  getSessions,
+  getSession,
+  addSession,
+  updateSession,
+  deleteSession,
+} = sessionSlice.actions;
 
 export default sessionSlice.reducer;
