8de64b3b5e90d36d855d6b52227d05a0cb56fbd7
sudhakarmako <sudhakar@makoitlab.com>
2022-06-03 02:39:04 +0530

    auto complete update

--- src/@ui/AutoComplete/AutoComplete.tsx
+++ src/@ui/AutoComplete/AutoComplete.tsx
@@ -5,7 +5,7 @@ const AutoComplete = ({
   data,
   onSelected,
   placeholder,
-  searchKey
+  searchKey,
 }: {
   data: [] | never[];
   onSelected: ({}) => void;
@@ -13,17 +13,19 @@ const AutoComplete = ({
   searchKey: string[];
 }) => {
   const [localData, setLocalData] = useState<[] | never[]>([]);
-  const [searchText, setSearchText] = useState();
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     !!data.length && setLocalData(data);
   }, [data]);
 
   useEffect(() => {
-    if(searchText){
-      const filterData = localData.filter((i:any) => i[searchKey[0]].includes(searchText));
+    if (searchText) {
+      const filterData = localData.filter((i: any) =>
+        i[searchKey[0]].includes(searchText)
+      );
       setLocalData(filterData);
-    }else{
+    } else {
       setLocalData(data);
     }
   }, [searchText]);
@@ -35,17 +37,21 @@ const AutoComplete = ({
           type="text"
           placeholder={placeholder}
           onFocus={() => setLocalData(data)}
+          value={searchText}
           onChange={(e: any) => setSearchText(e.target.value)}
         />
         <i className="ac-icon bi bi-search"></i>
       </div>
       {searchText && !!localData.length && (
         <div className="auto-options">
-          {localData.map((option:any, key) => (
+          {localData.map((option: any, key) => (
             <button
               key={key}
               className="auto-option"
-              onClick={() => onSelected(option)}
+              onClick={() => {
+                onSelected(option);
+                setSearchText("");
+              }}
             >
               {option[searchKey[0]]}
             </button>
