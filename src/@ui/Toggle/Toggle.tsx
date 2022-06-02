5333d95bb9a3025107f00d8c0d827059ee371b5b
sudhakarmako <sudhakar@makoitlab.com>
2022-06-02 10:45:57 +0530

    toggle logic updated

--- src/@ui/Toggle/Toggle.tsx
+++ src/@ui/Toggle/Toggle.tsx
@@ -5,7 +5,7 @@ type ToggleType = {
   id?: string;
   label?: string;
   name?: string;
-  onChange: React.Dispatch<React.SetStateAction<boolean>>;
+  onChange: ((value:boolean) => void);
   onText?: string | "Active";
   offText?: string | "In-Active";
   defaultChecked: boolean;
@@ -34,9 +34,9 @@ const Toggle = ({
           type="checkbox"
           name={name}
           checked={toggle}
-          onChange={e => {setToggle(e.target.checked); onChange(e.target.checked)}}
+          onChange={e => setToggle(e.target.checked)}
         />
-      <button className="toggle-container" type="button" onClick={() => setToggle(!toggle)}>
+      <button className="toggle-container" type="button" onClick={() => {setToggle(!toggle); onChange(!toggle)}}>
         <div className={`toggle-indicator ${toggle ? "on" : "off"}`}>
           <p>{onText}</p>
           <p>{offText}</p>
