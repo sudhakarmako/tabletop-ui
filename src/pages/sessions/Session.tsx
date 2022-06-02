bd97982a230ca50e6807ea51b606d6e5266437d5
sudhakarmako <sudhakar@makoitlab.com>
2022-06-02 18:57:54 +0530

    session view

--- src/pages/sessions/Session.tsx
+++ src/pages/sessions/Session.tsx
@@ -1,42 +1,88 @@
 import { Chip, Row } from "@ui";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { RootState } from "store";
 import "./Session.scss";
+import dayjs from "dayjs";
 
-type sessionDetail = {
-  sessionDetail?: number | null;
-};
+const Session = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const [session, setSession] = useState({
+    id: 0,
+    gameId: 0,
+    playerId: [],
+    created_at: "",
+    updated_at: "",
+    is_active: false,
+    game: {
+      title: "",
+      body: "",
+      authour: "",
+      created_at: "",
+      is_active: false,
+    },
+  });
+  const { sessions } = useSelector((state: RootState) => state.session);
+  const { players } = useSelector((state: RootState) => state.player);
 
-const Session = ({ sessionDetail }: sessionDetail) => {
+  useEffect(() => {
+    if (params && params.sessionId && sessions.length) {
+      const [selectedSession] = sessions.filter(
+        (ses: any) => ses.id === Number(params.sessionId)
+      );
+      setSession(selectedSession);
+    }
+  }, [params, sessions]);
+
   return (
     <div className="session-container">
       <Row>
-        <Chip>Session: 245</Chip>
+        <Chip
+          onClose={() => navigate("/sessions")}
+        >{`Session: #${session.id}`}</Chip>
       </Row>
-
-      <h2>Lost Ruins of Arnak (2020)</h2>
-      <p>
-        On an uninhabited island in uncharted seas, explorers have found traces
-        of a great civilization. Now you will lead an expedition to explore the
-        island, find lost artifacts, and face fearsome guardians, all in a quest
-        to learn the island's secrets.
-      </p>
-
+      <h2>{session.game.title}</h2>
+      <p>{session.game.body}</p>
+      <h4>More info:</h4>
+      <ul className="session-game-meta">
+        <li>
+          <strong>Authour : </strong>
+          {session.game.authour}
+        </li>
+        <li>
+          <strong>Created on : </strong>
+          {dayjs(session.game.created_at).format("DD-MMM-YYYY")}
+        </li>
+        <li>
+          <strong>Active : </strong>
+          {session.game.is_active ? "Yes" : "No"}
+        </li>
+      </ul>
       <h2 className="player">
         Players
         <span>
           <i className="bi bi-people"></i>
-          10
+          {session.playerId.length}
         </span>
       </h2>
       <Row>
-        {Array.apply(null, Array(10)).map(() => (
-          <Chip
-            image={
-              "https://cf.geekdo-images.com/3HkjDovk8Yr2wMumcSUGog__itemrep/img/WE_jrFpy57ekZuiIKFIMpNfXIXQ=/fit-in/246x300/filters:strip_icc()/pic4843622.jpg"
-            }
-          >
-            Player Name
-          </Chip>
-        ))}
+        {!!players.length &&
+          session.playerId.map((player: { id: number }, key) => {
+            const [chip]: {
+              first_name: string;
+              avatar: string;
+              last_name: string;
+            }[] = players.filter((pl: any) => pl.id === player.id);
+            return (
+              chip && (
+                <Chip key={key} image={chip.avatar}>
+                  {`${chip.first_name} ${chip.last_name}`}
+                </Chip>
+              )
+            );
+          })}
       </Row>
     </div>
   );
