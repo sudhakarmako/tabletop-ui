988d9b81bba932c46ed965e8d6ad2f3e0f632564
sudhakarmako <sudhakar@makoitlab.com>
2022-06-02 12:08:29 +0530

    onclick on button

--- src/@ui/Button/Button.tsx
+++ src/@ui/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import "./Button.scss";
-const Button = ({ children, type }: { children: ReactNode, type?:"button" | "submit" | "reset" | undefined }) => {
-  return <button className="button" type={type}>{children}</button>;
+const Button = ({ children, type, onClick }: { children: ReactNode, type?:"button" | "submit" | "reset" | undefined, onClick?:(() => void) }) => {
+  return <button className="button" onClick={onClick} type={type}>{children}</button>;
 };
 export default Button;
